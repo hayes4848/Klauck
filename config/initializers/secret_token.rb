fc1da59d4258a7e70cbfffd0f0bfc19b2a5660d6
Andy Hayes <andythemanhayes@hotmail.com>
2014-04-22 19:03:51 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Klauck::Application.config.secret_token = '7bb5c1ff5873f16456b68abcf29db919916e4073826a2819e013af991fe465a80678b8c657c5041cb87fc6880ca2e83e88f6ee5772d8936a8bc043dcb5714d40'
