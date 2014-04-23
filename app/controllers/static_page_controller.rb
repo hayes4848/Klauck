b9056c72e342d0891c64e5361f8fc8b165017d98
Andy Hayes <andythemanhayes@hotmail.com>
2014-04-22 19:28:06 -0600

    initial commit

---
+++ app/controllers/static_page_controller.rb
@@ -0,0 +1,11 @@
+class StaticPageController < ApplicationController
+
+def Home
+  	
+  end
+
+  def About
+  	
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Klauck::Application.routes.draw do
   devise_for :users
+
+get "/home", to: "static_page#Home", as:  :home
+
+root to: "static_page#Home"
+
 end
+
